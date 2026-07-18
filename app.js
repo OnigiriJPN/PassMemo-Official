8fd71a33c3bc45be1fc4de0075e2f23f63dd25c2
OnigiriJPN <yuzuhajintian@gmail.com>
2026-07-18 13:50:55 +0900

    Add switchTab function to manage active tabs

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 window.addEventListener('blur', () => document.body.classList.add('mica-inactive'))
 window.addEventListener('focus', () => document.body.classList.remove('mica-inactive'))
 
+function switchTab(id) {
+  document.querySelectorAll('.page').forEach(p = () => p.classList.remove('active'));
+  document.getElementById(id + '-page').classList.add('active);
+}
