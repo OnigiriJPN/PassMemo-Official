6c7bdf02eb0309a7a39d1810270da4748799e346
OnigiriJPN <yuzuhajintian@gmail.com>
2026-07-18 13:55:17 +0900

    Add toggleCustom function for custom options display

--- app.js
+++ app.js
@@ -5,3 +5,8 @@ function switchTab(id) {
   document.querySelectorAll('.page').forEach(p = () => p.classList.remove('active'));
   document.getElementById(id + '-page').classList.add('active);
 }
+
+function toggleCustom() {
+  document.getElementById('custom-options').style.display =
+    (document.getElementById('level').value === 'custom') ? 'block' : 'none';
+}
